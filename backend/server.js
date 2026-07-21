ca80f6cc590cdea6abbc79abee0b06ce43bacdbd
Nehul <ng5551@srmist.edu.in>
2026-07-21 22:44:58 +0530

    feat: allow dynamic CORS origin via environment variable

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 // Configure CORS
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000",process.env.ALLOWED_CORS_ORIGIN],
     credentials: true,
   })
 );
